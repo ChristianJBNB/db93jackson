890dde06fc70ad07bf530c98074531a2f7b19329
Christian Jackson <cjackson430@yahoo.com>
2021-11-11 09:40:54 -0600

    Completed Part 1 of Assignment 12

--- controllers/player.js
+++ controllers/player.js
@@ -32,8 +32,15 @@ exports.player_list = function(req, res) {
 }; 
  
 // for a specific Player. 
-exports.player_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Player detail: ' + req.params.id); 
+exports.player_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await player.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
 }; 
  
 // Handle Player create on POST. 
